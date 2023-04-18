c45a43452b060fc5a64a768100a436e115a9b210
gesellkammer <eduardo.moguillansky@gmail.com>
2023-04-18 17:41:16 +0200

    debug logger

--- test/test1.py
+++ test/test1.py
@@ -1,4 +1,9 @@
-import lilyponddist
+import logging
 import subprocess
+
+logger = logging.getLogger('lilyponddist')
+logger.setLevel('DEBUG')
+
+import lilyponddist
 
 subprocess.call([lilyponddist.lilypondbin(), "test1.ly"])
