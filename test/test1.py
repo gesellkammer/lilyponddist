aee2f73c79ec2f929294ca98690b976ebb336f2e
gesellkammer <eduardo.moguillansky@gmail.com>
2023-04-18 17:59:13 +0200

    test

--- test/test1.py
+++ test/test1.py
@@ -1,9 +1,13 @@
 import logging
 import subprocess
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('--output', default='test1')
+args = parser.parse_args()
 
 logger = logging.getLogger('lilyponddist')
 logger.setLevel('DEBUG')
 
 import lilyponddist
 
-subprocess.call([lilyponddist.lilypondbin(), "test1.ly"])
+subprocess.call([lilyponddist.lilypondbin(), "test1.ly", '-o', args.output])
